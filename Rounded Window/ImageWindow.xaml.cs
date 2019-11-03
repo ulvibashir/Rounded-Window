249b385c0b9dbb0e5a0002084a71b2fcce86fdfa
Ulvi Bashir <ubashirov@outlook.com>
2019-11-03 20:24:24 +0400

    All done

---
+++ Rounded Window/ImageWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Rounded_Window
+{
+    /// <summary>
+    /// Interaction logic for ImageWindow.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class ImageWindow : Window
+    {
+        public string ImagePath { get; set; }
+
+
+        public ImageWindow()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        public ImageWindow(string path)
+        {
+            this.DataContext = this;
+            ImagePath = path;
+            InitializeComponent();
+        }
+
+      
+
+
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+        private void ContentControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Rounded Window/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rounded_Window
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Environment.Exit(0);
+        }
+        /*
+         * Double click the picture to open new window
+         * And double click to new window to close it 
+         * 
+         * 
+         * */
+        private void ContentControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ImageWindow imageWindow = new ImageWindow("Images/img1.jpg");
+            imageWindow.Show();
+        }
+        private void ContentControl_MouseDoubleClick2(object sender, MouseButtonEventArgs e)
+        {
+            ImageWindow imageWindow = new ImageWindow("Images/img2.jpg");
+            imageWindow.Show();
+        }
+        private void ContentControl_MouseDoubleClick3(object sender, MouseButtonEventArgs e)
+        {
+            ImageWindow imageWindow = new ImageWindow("Images/img3.jpg");
+            imageWindow.Show();
+        }
+        private void ContentControl_MouseDoubleClick4(object sender, MouseButtonEventArgs e)
+        {
+            ImageWindow imageWindow = new ImageWindow("Images/img4.jpg");
+            imageWindow.Show();
+        }
+    }
+}
